1df65d75f5cdbfd6ab64097e14a8b14c5bdde615
ryo-imai-bit <mayoimaimai9@gmail.com>
2021-10-25 20:25:00 +0900

    totyu

--- lib/parser.ml
+++ lib/parser.ml
@@ -7,6 +7,7 @@ module Parser = struct
     l: Lexer.lexer;
     curToken: Token.token;
     peekToken: Token.token;
+    errors: string list;
   }
 
   let newParser lex = let (le, tok) = Lexer.nextToken lex
@@ -14,7 +15,8 @@ module Parser = struct
     in {
       l = le;
       curToken = tok;
-      peekToken = tk
+      peekToken = tk;
+      errors = [];
     }
 
   let nextToken prs = let (le, _) = Lexer.nextToken prs.l
@@ -22,12 +24,33 @@ module Parser = struct
       l = le;
       curToken = prs.peekToken;
       peekToken = t;
+      errors = prs.errors;
     }
 
-  let parseLetStatement prs = (nextToken prs |> nextToken |> nextToken |> nextToken, Ast.LetStatment {idt = Ast.Identifier "a"; value = Ast.IntegerLiteral 1;})
+  let parseToEnd prs = let rec toEnd pr = match pr.curToken with
+  | {literal = _; t_type = Token.EOF} -> pr
+  | _ -> toEnd pr
+  in toEnd prs
+
+  let parseIntegerLiteral prs = (nextToken prs, Ast.IntegerLiteral (int_of_string prs.curToken.literal))
+
+  let parseIdentifier prs = (nextToken prs, Ast.Identifier prs.curToken.literal)
+
+  let parseStringLiteral prs = (nextToken prs, Ast.StringLiteral prs.curToken.literal)
+
+  let parseExpression prs = match prs.curToken with
+  | {literal = _; t_type = Token.INT} -> parseIntegerLiteral prs
+  | {literal = _; t_type = Token.IDENT} -> parseIdentifier prs
+  | {literal = _; t_type = Token.STRING} -> parseStringLiteral prs
+  | _ -> parseStringLiteral prs
+
+  (* let parseLetStatement prs = (nextToken prs |> nextToken |> nextToken |> nextToken, Ast.LetStatment {idt = Ast.Identifier "a"; value = Ast.IntegerLiteral 1;}) *)
+  let parseLetStatement prs = match prs.curToken with
+  | {literal = _; t_type = Token.IDENT} -> (prs)
+  | {literal; t_type} -> let errp = parseToEnd prs in {l = errp.l; curToken = errp.curToken; peekToken = errp.peekToken ;errors = prs.errors @ ["litera: "literal ^ " token: " ^ Token.tokenToString t_type]}
 
   let parseStatement prs = match prs.curToken with
-  | {literal = "let"; t_type = Token.LET} -> parseLetStatement prs
+  | {literal = "let"; t_type = Token.LET} -> nextToken prs |> parseLetStatement
   | _ -> (nextToken prs, Ast.ReturnStatement)
   (* let parseProgram _ _ = [Ast.LetStatment {idt = Ast.Identifier "a"; value = Ast.IntegerLiteral 1}] *)
 
