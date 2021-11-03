14df5794d983526109d32ca6644287d222b14bab
ryo-imai-bit <mayoimaimai9@gmail.com>
2021-11-04 01:04:59 +0900

    add repl

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,4 @@
 (* let () = print_endline Interpreter.Lexer.a *)
+include Interpreter
+
+let () = try while true do Interpreter.run () done with End_of_file -> ()
--- lib/interpreter.ml
+++ lib/interpreter.ml
@@ -6,3 +6,23 @@ module Evaluator = Evaluator.Evaluator
 module Object = Object.Object
 module Env = Env.Env
 module Builtin = Builtin.Builtin
+
+let prompt = ">>"
+let monkey = {|"
+          __,__
+.--.  .-"     "-.  .--.
+/ .. \/  .-. .-.  \/ .. \
+| |  '|  /   Y   \  |'  | |
+| \   \  \ 0 | 0 /  /   / |
+\ '- ,\.-"""""""-./, -' /
+''-' /_   ^ ^   _\ '-''
+    |  \._   _./  |
+    \   \ '~' /   /
+      '._ '-=-' _.'
+        '-----"|}
+
+
+let run () = print_string prompt;
+  let input = read_line ()
+  in let prg = Parser.parseProgram (Parser.newParser (Lexer.newLexer input)) []
+  in print_endline (Object.objToString (Evaluator.evalProgram prg (Env.newEnv)))
