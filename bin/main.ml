6cf0b0fbbd94439e687063acaa28ac914b49cd1b
ryo-imai-bit <mayoimaimai9@gmail.com>
2021-11-08 14:31:10 +0900

    add message to repl

--- bin/main.ml
+++ bin/main.ml
@@ -1,4 +1,7 @@
 (* let () = print_endline Interpreter.Lexer.a *)
 include Interpreter
 
-let () = try while true do Interpreter.run () done with End_of_file -> ()
+let () =
+  print_endline "HEllo! This is the Monkey programming language!\n";
+  print_endline "Feel free to type in commands\n";
+  try while true do Interpreter.run () done with End_of_file -> ()
